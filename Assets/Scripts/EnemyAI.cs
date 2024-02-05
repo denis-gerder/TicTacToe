6949537ff6b46c627e84caa3ca4d3921a04c36a7
Dönis <denesgerder@gmail.com>
2024-02-05 10:18:49 +0100

    added correct player win prevention

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -67,7 +67,7 @@ public class EnemyAI : TileHandler
     {
         bool playerWinPrevented = false;
 
-        playerWinPrevented = CheckForCloseHorizontalPlayerWin();
+        playerWinPrevented = CheckForCloseHorizontalPlayerWin() || CheckForCloseVericalPlayerWin() || CheckForCloseDiagonalWin();
         
         return playerWinPrevented;
     }
@@ -75,6 +75,34 @@ public class EnemyAI : TileHandler
     private void PlaceSmartTile()
     {
         
+    }
+    
+    private bool CheckForCloseVericalPlayerWin()
+    {
+        Vector2Int emptyTilePosition = new Vector2Int(0, 0);
+        for (int row = 0; row < Grid.Instance.GridWidth; row++)
+        {
+            int playerTilesInCol = 0;
+            
+            for (int col = 0; col < Grid.Instance.GridWidth; col++)
+            {
+                if(Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[row, col]] == null) emptyTilePosition = new Vector2Int(row, col);
+                
+                //check if tile is placed by player and not empty
+                else if(playerConfigSO.PlayerSymbols[0] == Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[row, col]].GetComponent<Image>().sprite) playerTilesInCol++;
+                
+                
+            }
+            if (playerTilesInCol == Grid.Instance.GridWidth - 1)
+            {
+                
+                GameObject enemyAITile = Instantiate(playerPrefab, Grid.Instance.TileMatrix[emptyTilePosition.x, emptyTilePosition.y].transform);
+                enemyAITile.GetComponent<Image>().sprite = playerConfigSO.PlayerSymbols[GameManager.Instance.currentPlayer - 1];
+                Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[emptyTilePosition.x, emptyTilePosition.y]] = enemyAITile;
+                return true;
+            }
+        }
+        return false;
     }
     
     private bool CheckForCloseHorizontalPlayerWin()
@@ -86,23 +114,59 @@ public class EnemyAI : TileHandler
             
             for (int col = 0; col < Grid.Instance.GridWidth; col++)
             {
-                if(Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[row, col]] == null) emptyTilePosition = new Vector2Int(row, col);
+                if(Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[col, row]] == null) emptyTilePosition = new Vector2Int(col, row);
                 
                 //check if tile is placed by player and not empty
-                else if(playerConfigSO.PlayerSymbols[0] == Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[row, col]].GetComponent<Image>().sprite) playerTilesInRow++;
+                else if(playerConfigSO.PlayerSymbols[0] == Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[col, row]].GetComponent<Image>().sprite) playerTilesInRow++;
                 
                 
             }
-
             if (playerTilesInRow == Grid.Instance.GridWidth - 1)
             {
-                
                 GameObject enemyAITile = Instantiate(playerPrefab, Grid.Instance.TileMatrix[emptyTilePosition.x, emptyTilePosition.y].transform);
                 enemyAITile.GetComponent<Image>().sprite = playerConfigSO.PlayerSymbols[GameManager.Instance.currentPlayer - 1];
                 Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[emptyTilePosition.x, emptyTilePosition.y]] = enemyAITile;
                 return true;
             }
         }
+        return false;
+    }
+    
+    private bool CheckForCloseDiagonalWin()
+    {
+        Vector2Int emptyTilePosition1 = new Vector2Int(0, 0);
+        int playerTilesInDgl1 = 0;
+
+        for (int row = 0; row < Grid.Instance.GridWidth; row++)
+        {
+            if (Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[row, row]] == null) emptyTilePosition1 = new Vector2Int(row, row);
+            else if (playerConfigSO.PlayerSymbols[0] == Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[row, row]].GetComponent<Image>().sprite) playerTilesInDgl1++;
+        }
+        
+        if (playerTilesInDgl1 == Grid.Instance.GridWidth - 1)
+        {
+            GameObject enemyAITile = Instantiate(playerPrefab, Grid.Instance.TileMatrix[emptyTilePosition1.x, emptyTilePosition1.y].transform);
+            enemyAITile.GetComponent<Image>().sprite = playerConfigSO.PlayerSymbols[GameManager.Instance.currentPlayer - 1];
+            Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[emptyTilePosition1.x, emptyTilePosition1.y]] = enemyAITile;
+            return true;
+        }
+        
+        Vector2Int emptyTilePosition2 = new Vector2Int(0, 0);
+        int playerTilesInDgl2 = 0;
+        for (int row = 0; row < Grid.Instance.GridWidth; row++)
+        {
+            if (Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[row, Grid.Instance.GridWidth - row - 1]] == null) emptyTilePosition2 = new Vector2Int(row, Grid.Instance.GridWidth - row - 1);
+            else if (playerConfigSO.PlayerSymbols[0] == Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[row, Grid.Instance.GridWidth - row - 1]].GetComponent<Image>().sprite) playerTilesInDgl2++;
+        }
+        
+        if (playerTilesInDgl2 == Grid.Instance.GridWidth - 1)
+        {
+            GameObject enemyAITile = Instantiate(playerPrefab, Grid.Instance.TileMatrix[emptyTilePosition2.x, emptyTilePosition2.y].transform);
+            enemyAITile.GetComponent<Image>().sprite = playerConfigSO.PlayerSymbols[GameManager.Instance.currentPlayer - 1];
+            Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[emptyTilePosition2.x, emptyTilePosition2.y]] = enemyAITile;
+            return true;
+        }
+        
         return false;
     }
      
