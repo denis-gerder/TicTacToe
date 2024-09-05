34ce2a3dc2dad60f4cf9d8f2c722d6e6f08018f5
Dönis <denesgerder@gmail.com>
2024-09-05 22:04:03 +0200

    alpha-beta pruning works and added multithreaded calculation for best tile

---
+++ Assets/Scripts/BoardTree.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+
+namespace Assets.Scripts
+{
+    public class BoardTree
+    {
+
+        public int[,] data;
+        private BoardTree parent;
+        private LinkedList<BoardTree> children;
+        private String StringRepresentation;
+        public int score = 1000;
+        public int alpha = 1000;
+        public int beta = 1000;
+
+        private static int idTracker = 0;
+
+        private int id;
+
+
+        public BoardTree()
+        {
+            this.data = null;
+            this.parent = null;
+            children = new LinkedList<BoardTree>();
+            id = idTracker;
+            idTracker++;
+        }
+
+        public BoardTree(int[,] data)
+        {
+            this.data = data;
+            this.parent = null;
+            children = new LinkedList<BoardTree>();
+            id = idTracker;
+            idTracker++;
+        }
+
+        public BoardTree AddChild(int[,] data)
+        {
+            BoardTree child = new BoardTree(data);
+            children.AddLast(child);
+            child.SetParent(this);
+
+            return child;
+        }
+
+        public BoardTree GetChild(int i)
+        {
+            foreach (BoardTree childTree in children)
+                if (--i == 0)
+                    return childTree;
+            return null;
+        }
+
+        public BoardTree SetParent(BoardTree parent)
+        {
+            this.parent = parent;
+            return this;
+        }
+
+        public BoardTree GetParent()
+        {
+            return parent;
+        }
+
+        public String PrintTree()
+        {
+            StringRepresentation = "";
+            Dictionary<int, List<BoardTree>> nodesPerDepth = new Dictionary<int, List<BoardTree>>();
+            PutNodesForDepths(this, nodesPerDepth, 0);
+            for (int depth = nodesPerDepth.Count - 1; depth >= 1; depth--)
+            {
+                nodesPerDepth.TryGetValue(depth, out List<BoardTree> list);
+                String LastRows = StringRepresentation;
+                StringRepresentation = PrintTreeAtDepth(list) + "\n" + LastRows;
+            }
+            return StringRepresentation;
+        }
+
+        public String PrintTreeAtDepth(List<BoardTree> nodesAtDepth)
+        {
+            StringRepresentation = "";
+            for (int i = nodesAtDepth[0].data.GetLength(0); i >= -3; i--)
+            {
+
+                BoardTree parent = new BoardTree();
+                foreach (BoardTree child in nodesAtDepth)
+                {
+                    if (parent != child.GetParent())
+                    {
+                        StringRepresentation += "|      ";
+                    }
+                    parent = child.GetParent();
+
+                    if (i == nodesAtDepth[0].data.GetLength(0))
+                    {
+                        String idString = child.id + ":" + parent.id;
+                        int stringLengthPerBoard = nodesAtDepth[0].data.GetLength(0) * 2 + 5;
+
+                        StringRepresentation += idString;
+
+                        for (int j = 0; j < stringLengthPerBoard - (int)(idString.Length * 1.2); j++)
+                        {
+                            StringRepresentation += " ";
+                        }
+                    }
+                    else if (i == -1)
+                    {
+                        String scoreString = "Sc: " + child.score;
+                        int stringLengthPerBoard = nodesAtDepth[0].data.GetLength(0) * 2 + 5;
+                        if (child.children.Count == 0)
+                        {
+                            scoreString += "T";
+                        }
+
+                        StringRepresentation += scoreString;
+
+                        for (int j = 0; j < stringLengthPerBoard - (int)(scoreString.Length * 1.17); j++)
+                        {
+                            StringRepresentation += " ";
+                        }
+                    }
+                    else if (i == -2)
+                    {
+                        String scoreString = "a: " + child.alpha;
+                        int stringLengthPerBoard = nodesAtDepth[0].data.GetLength(0) * 2 + 5;
+
+                        StringRepresentation += scoreString;
+
+                        for (int j = 0; j < stringLengthPerBoard - (int)(scoreString.Length * 1.17); j++)
+                        {
+                            StringRepresentation += " ";
+                        }
+                    }
+                    else if (i == -3)
+                    {
+                        String scoreString = "b: " + child.beta;
+                        int stringLengthPerBoard = nodesAtDepth[0].data.GetLength(0) * 2 + 5;
+
+                        StringRepresentation += scoreString;
+
+                        for (int j = 0; j < stringLengthPerBoard - (int)(scoreString.Length * 1.17); j++)
+                        {
+                            StringRepresentation += " ";
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 0; j < child.data.GetLength(1); j++)
+                        {
+                            StringRepresentation += child.data[i, j] + " ";
+                        }
+                        StringRepresentation += "     ";
+                    }
+
+                }
+                StringRepresentation += "\n";
+            }
+
+            return StringRepresentation;
+        }
+
+        private void PutNodesForDepths(BoardTree node, Dictionary<int, List<BoardTree>> nodesPerDepth, int depth)
+        {
+            if (!nodesPerDepth.ContainsKey(depth))
+            {
+                nodesPerDepth.Add(depth, new List<BoardTree>());
+            }
+            nodesPerDepth[depth].Add(node);
+            foreach (BoardTree child in node.children)
+            {
+                PutNodesForDepths(child, nodesPerDepth, depth + 1);
+            }
+        }
+    }
+}
--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,8 +1,16 @@
+using Assets.Scripts;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 using Random = UnityEngine.Random;
 
 public class EnemyAI : TileHandler
@@ -12,20 +20,20 @@ public class EnemyAI : TileHandler
     private Dictionary<String, int> _scores;
 
     private int _currentPlayerToSimulate;
-    
+
     private int _roundToSimulate;
-    
+
     protected void Awake()
     {
         Grid.OnTurnEnd += HandleTurnEnd;
         Grid.OnGameOver += HandleGameOver;
     }
-    
+
     private void HandleTurnEnd()
     {
         //return if AI is disabled or if it's not the AI's turn
         if (!GameManager.Instance.IsAIEnabled || GameManager.Instance.currentPlayer == 1 || _gameOver) return;
-        
+
         //ai places tile
         switch (aiDifficulty)
         {
@@ -36,57 +44,118 @@ public class EnemyAI : TileHandler
                 SmartAI();
                 break;
         }
-        
+
         //turn ends and next player is set
-        int nextPlayer = GameManager.Instance.currentPlayer != GameManager.Instance.PlayerCount ? GameManager.Instance.currentPlayer + 1 : 1;
+        int nextPlayer = GameManager.Instance.currentPlayer != GameManager.Instance.PlayerCount
+            ? GameManager.Instance.currentPlayer + 1
+            : 1;
         GameManager.Instance.currentPlayer = nextPlayer;
         PlayerPlaced();
     }
 
+    struct TileScore
+    {
+        public int row;
+        public int col;
+        public int score;
+
+        public TileScore(int row, int col, int score)
+        {
+            this.row = row;
+            this.col = col;
+            this.score = score;
+        }
+    }
+
+    struct AsyncProps
+    {
+        public int row;
+        public int col;
+        public BoardState boardStateCopy;
+        public List<TileScore> tileScores;
+
+        public AsyncProps(int row, int col, BoardState boardStateCopy, List<TileScore> tileScores)
+        {
+            this.row = row;
+            this.col = col;
+            this.boardStateCopy = boardStateCopy;
+            this.tileScores = tileScores;
+        }
+    }
+
     private void SmartAI()
     {
-        int bestScore = int.MinValue;
         //store best move to place next tile as row and col
         Dictionary<String, int> bestMove = new();
         //create simple copy of the board state as integers to reduce complexity of placing and removing tiles as game objects
-        BoardState boardStateCopy = new(Grid.Instance.TileMatrix, Grid.Instance.PlayerPerTile);
-        
+        BoardState originalBoardState = new(Grid.Instance.TileMatrix, Grid.Instance.PlayerPerTile);
+
+        BoardState boardStateCopy = new(originalBoardState);
+
+        List<TileScore> tileScores = new();
+        object listLock = new object();
+
+        var executionTracker = Stopwatch.StartNew();
+
         for(int row = 0; row < Grid.Instance.GridWidth; row++)
         {
             for(int col = 0; col < Grid.Instance.GridWidth; col++)
             {
                 //skip if tile is not empty
-                if (Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[row, col]] != null) continue;
-                
-                //set current player and round for copied board state to simulate next move
-                boardStateCopy.CurrentPlayer = GameManager.Instance.currentPlayer;
-                boardStateCopy.CurrentRound = GameManager.Instance.round+1;
-                
-                //place tile at current test position in copied board
-                boardStateCopy.Board[row, col] = boardStateCopy.CurrentPlayer;
+                if (Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[row, col]] != null) 
+                    continue;
 
-                if(GameManager.Instance.EnableLogging)
-                    boardStateCopy.AddStateToDebugString();
-
-                //get score for that new board state
-                int score = MiniMax(boardStateCopy, 0, false, boardStateCopy.CurrentPlayer, boardStateCopy.CurrentRound, int.MinValue, int.MaxValue);
-                
-                //remove reference from copied board to reverse changes
-                boardStateCopy.Board[row, col] = 0;
-                
-                //update best score and best move
-                if(score > bestScore) {
-                    bestScore = score;
-                    bestMove["row"] = row;
-                    bestMove["col"] = col;
-                }
-                if (GameManager.Instance.EnableLogging)
+                ThreadPool.QueueUserWorkItem(new WaitCallback(asyncProps =>
                 {
-                    boardStateCopy.placeSequenceString += " Best Score: " + bestScore + "\n";
-                    Debug.Log(boardStateCopy.placeSequenceString);
-                    boardStateCopy.placeSequenceString = "";
-                }
+                    //set current player and round for copied board state to simulate next move
+                    ((AsyncProps)asyncProps).boardStateCopy.CurrentPlayer = GameManager.Instance.currentPlayer;
+                    ((AsyncProps)asyncProps).boardStateCopy.CurrentRound = GameManager.Instance.round + 1;
+
+                    //place tile at current test position in copied board
+                    ((AsyncProps)asyncProps).boardStateCopy.Board[((AsyncProps)asyncProps).row, ((AsyncProps)asyncProps).col] = ((AsyncProps)asyncProps).boardStateCopy.CurrentPlayer;
+
+                    BoardTree currentNode = ((AsyncProps)asyncProps).boardStateCopy.currentNode;
+                    if (GameManager.Instance.EnableLogging)
+                        ((AsyncProps)asyncProps).boardStateCopy.AddStateToTree();
+
+                    //get score for that new board state
+                    int score = MiniMax(((AsyncProps)asyncProps).boardStateCopy, 0, false, ((AsyncProps)asyncProps).boardStateCopy.CurrentPlayer, ((AsyncProps)asyncProps).boardStateCopy.CurrentRound, -10, 10);
+
+                    //remove reference from copied board to reverse changes
+                    ((AsyncProps)asyncProps).boardStateCopy.Board[((AsyncProps)asyncProps).row, ((AsyncProps)asyncProps).col] = 0;
+
+                    lock (listLock)
+                        ((AsyncProps)asyncProps).tileScores.Add(new TileScore(((AsyncProps)asyncProps).row, ((AsyncProps)asyncProps).col, score));
+                }), new AsyncProps(row, col, new BoardState(originalBoardState), tileScores));
             }
+        }
+
+        bool working = true;
+        ThreadPool.GetMaxThreads(out int maxWorkerThreads, out int maxCompletionPortThreads);
+        while (working)
+        {
+            ThreadPool.GetAvailableThreads(out int workerThreads, out int completionPortThreads);
+            if (workerThreads == maxWorkerThreads)
+            { working = false; }
+        }
+        TileScore? bestTile = null;
+        foreach (var tileScore in tileScores)
+        {
+            if (bestTile == null || tileScore.score > bestTile.Value.score)
+            {
+                bestTile = tileScore;
+            }
+        }
+
+        executionTracker.Stop();
+        Debug.Log("Execution time: " + executionTracker.ElapsedMilliseconds + "ms");
+
+        bestMove["row"] = bestTile.Value.row;
+        bestMove["col"] = bestTile.Value.col;
+
+        if (GameManager.Instance.EnableLogging)
+        {
+            //boardStateCopy.PrintTree();
         }
         GameObject bestMoveTile = Grid.Instance.TileMatrix[bestMove["row"], bestMove["col"]];
         GameObject enemyAITile = Instantiate(playerPrefab, bestMoveTile.transform.GetChild(0));
@@ -100,21 +169,17 @@ public class EnemyAI : TileHandler
 
         if (winner == 0 && board.CurrentRound == board.Board.GetLength(0) * board.Board.GetLength(1))
         {
-            if (GameManager.Instance.EnableLogging)
-                board.AddStateToDebugString(0);
             return 0;
         }
         if (winner != 0)
         {
             int score = winner == GameManager.Instance.currentPlayer ? 1 : -1;
-            if (GameManager.Instance.EnableLogging)
-                board.AddStateToDebugString(score);
             return score;
         }
         
         if (isMaximizing)
         {
-            int bestScore = int.MinValue;
+            int bestScore = -10;
             for (int row = 0; row < board.Board.GetLength(0); row++)
             {
                 for (int col = 0; col < board.Board.GetLength(0); col++)
@@ -127,28 +192,37 @@ public class EnemyAI : TileHandler
                     //place tile and get score for that new board state
                     board.Board[row, col] = board.CurrentPlayer;
 
+                    BoardTree currentNode = board.currentNode;
                     if (GameManager.Instance.EnableLogging)
-                        board.AddStateToDebugString();
+                        board.AddStateToTree();
 
-                    int score = MiniMax(board, depth + 1, false, board.CurrentPlayer, board.CurrentRound, int.MinValue, int.MaxValue);
-                    
+                    int score = MiniMax(board, depth + 1, false, board.CurrentPlayer, board.CurrentRound, alpha, beta);
+                    board.currentNode.score = score;
+                    board.currentNode = currentNode;
+
                     //destroy tile and remove reference from grid to reverse changes
                     board.Board[row, col] = 0;
                     
                     //update best score
                     bestScore = Math.Max(score, bestScore);
                     alpha = Math.Max(alpha, bestScore);
+                    currentNode.alpha = alpha;
+                    currentNode.beta = beta;
                     if (beta <= alpha)
                     {
                         break;
                     }
+                }
+                if (beta <= bestScore)
+                {
+                    break;
                 }
             }
             return bestScore;
         }
         else
         {
-            int bestScore = int.MaxValue;
+            int bestScore = 10;
             for (int row = 0; row < board.Board.GetLength(0); row++)
             {
                 for (int col = 0; col < board.Board.GetLength(0); col++)
@@ -161,21 +235,30 @@ public class EnemyAI : TileHandler
                     //place tile and get score for that new board state
                     board.Board[row, col] = board.CurrentPlayer;
 
+                    BoardTree currentNode = board.currentNode;
                     if (GameManager.Instance.EnableLogging)
-                        board.AddStateToDebugString();
-
-                    int score = MiniMax(board, depth + 1, true, board.CurrentPlayer, board.CurrentRound, int.MinValue, int.MaxValue);
+                        board.AddStateToTree();
                     
+                    int score = MiniMax(board, depth + 1, true, board.CurrentPlayer, board.CurrentRound, alpha, beta);
+                    board.currentNode.score = score;
+                    board.currentNode = currentNode;
+
                     //destroy tile and remove reference from grid to reverse changes
                     board.Board[row, col] = 0;
                     
                     //update best score
                     bestScore = Math.Min(score, bestScore);
                     beta = Math.Min(beta, bestScore);
+                    currentNode.alpha = alpha;
+                    currentNode.beta = beta;
                     if (beta <= alpha)
                     {
                         break;
                     }
+                }
+                if (bestScore <= alpha)
+                {
+                    break;
                 }
             }
             return bestScore;
@@ -190,7 +273,8 @@ public class EnemyAI : TileHandler
         {
             for(int col = 0; col < Grid.Instance.GridWidth; col++)
             {
-                if(Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[row, col]] == null) emptyTiles.Add(Grid.Instance.TileMatrix[row, col]);
+                if(Grid.Instance.PlayerPerTile[Grid.Instance.TileMatrix[row, col]] == null) 
+                    emptyTiles.Add(Grid.Instance.TileMatrix[row, col]);
             }
         }
         //get random empty tile and place player tile with reference in grid
@@ -210,8 +294,11 @@ public class BoardState
     
     public int CurrentPlayer;
 
-    public String placeSequenceString;
-    
+    private BoardTree root;
+
+    public BoardTree currentNode;
+
+
     public BoardState(GameObject[,] tileMatrix, Dictionary<GameObject, GameObject> playerPerTile)
     {
         Board = new int[tileMatrix.GetLength(0), tileMatrix.GetLength(1)];
@@ -222,7 +309,30 @@ public class BoardState
                 Board[row, col] = playerPerTile[tileMatrix[row, col]] == null ? 0 : Int32.Parse(playerPerTile[tileMatrix[row, col]].GetComponent<Image>().sprite.name[6].ToString());
             }
         }
+        root = new BoardTree();
+        currentNode = root;
     } 
+
+    public BoardState(BoardState boardState)
+    {
+        Board = (int[,])boardState.Board.Clone();
+        CurrentRound = boardState.CurrentRound;
+        CurrentPlayer = boardState.CurrentPlayer;
+        root = new BoardTree();
+        currentNode = root;
+    }
+
+    public void AddStateToTree()
+    {
+        currentNode = currentNode.AddChild(CopyBoardState());
+    }
+
+    public int[,] CopyBoardState() => (int[,]) Board.Clone();
+
+    public void PrintTree()
+    {
+        Debug.Log(root.PrintTree());
+    }
 
     public int CheckForWin()
     {
@@ -318,7 +428,6 @@ public class BoardState
     public void AddStateToDebugString(int score)
     {
 
-        placeSequenceString += " Score:" + score + "\n------\n";
     }
 
     public void AddStateToDebugString()
@@ -333,8 +442,6 @@ public class BoardState
             }
             boardString = rowString + "\n" + boardString;
         }
-
-        placeSequenceString += boardString + "------\n";
     }
 }
 
